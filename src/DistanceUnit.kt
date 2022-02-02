5a9cf7f1712b7e11f8b2a93878af61eae8d7061c
Mike Reynolds <Mike Reynolds@MREYNOLDS>
2022-02-01 20:42:25 -0600

    In Progress Commit

--- src/DistanceUnit.kt
+++ src/DistanceUnit.kt
@@ -1,9 +1,13 @@
 /**
  * Enumeration of Types of Distance Units
+ *
  * @property [displayName] is a string value of a given type
  */
 enum class DistanceUnit(private val displayName: String) {
-    FEET("Feet"), MILES("Miles"), METERS("Meters"), KILOMETERS("Kilometers");
+    FEET("Feet"),
+    MILES("Miles"),
+    METERS("Meters"),
+    KILOMETERS("Kilometers");
 
     /**
      * Overrides toString method to return the [displayName].
--- src/flightModel/Distance.kt
+++ src/flightModel/Distance.kt
@@ -2,6 +2,12 @@ package flightModel
 
 import DistanceUnit
 
+/**
+ * Distance class to hold value and Distance Unit.
+ *
+ * @property [value] sets the value of Distance
+ * @property [distanceUnit] sets the Distance Unit of the value set
+ */
 data class Distance(
     val value: Double,
     val distanceUnit: DistanceUnit
